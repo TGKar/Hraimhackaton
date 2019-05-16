45633d6686864b9a85e69fa9ee9d1f6b7e71fcb0
adiSilber <adiSilber@users.noreply.github.com>
2019-05-16 17:40:09 +0300

    first driver view

--- app/src/main/java/com/example/hraimackaton/DriverScreen.java
+++ app/src/main/java/com/example/hraimackaton/DriverScreen.java
@@ -1,7 +1,10 @@
 package com.example.hraimackaton;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class DriverScreen extends AppCompatActivity {
@@ -10,7 +13,11 @@ public class DriverScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_screen);
-        TextView textView = findViewById(R.id.textViewDriver);
-        textView.setText("I DRIVE");
+//        TextView textView = findViewById(R.id.textViewDriver);
+//        textView.setText("I DRIVE");
+    }
+
+    public void sendDriverInfo(View view) {
+        EditText nameField = findViewById()
     }
 }
