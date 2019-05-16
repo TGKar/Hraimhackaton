20d80bbb6ce4a912dd55b27a75e58582872037bf
tutgadol <barkrv@gmail.com>
2019-05-16 19:53:55 +0300

    Updated driver scrreen

--- app/src/main/java/com/example/hraimackaton/DriverScreen.java
+++ app/src/main/java/com/example/hraimackaton/DriverScreen.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class DriverScreen extends AppCompatActivity {
@@ -13,11 +14,23 @@ public class DriverScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_screen);
+
 //        TextView textView = findViewById(R.id.textViewDriver);
 //        textView.setText("I DRIVE");
     }
 
     public void sendDriverInfo(View view) {
-        EditText nameField = findViewById()
+        EditText nameField = (EditText) findViewById(R.id.textViewDriverName);
+        String name = nameField.getText().toString();
+        EditText faceBookField = (EditText) findViewById(R.id.textViewFacebook);
+        String facebook = nameField.getText().toString();
+        EditText fromField = (EditText) findViewById(R.id.textViewFrom);
+        String from = fromField.getText().toString();
+//        EditText destinationField = (EditText) findViewById(R.id.textViewDestination);
+//        String destination = destinationField.getText().toString();
+//        EditText minLeaveField = (EditText) findViewById(R.id.textViewMinHour);
+//        minLeave
+//        TextView show = findViewById(R.id.textView);
+//        show.setText(name);
     }
 }
