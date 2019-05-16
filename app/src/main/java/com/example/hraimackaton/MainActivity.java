2ae4d22a5f52111ec59140b90561d0b2b9c74f50
tutgadol <barkrv@gmail.com>
2019-05-16 14:58:57 +0300

    I wrote something on Android

--- app/src/main/java/com/example/hraimackaton/MainActivity.java
+++ app/src/main/java/com/example/hraimackaton/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.hraimackaton;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,7 +18,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    void sendMessage() {
-        
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DriverScreen.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
+
+
 }
