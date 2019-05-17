1df4cf1605bb58af134cf664e4c371ff91ff452d
tutgadol <barkrv@gmail.com>
2019-05-17 05:38:04 +0300

    Fuck u

--- app/src/main/java/com/example/hraimackaton/TheDrive_riderSide.java
+++ app/src/main/java/com/example/hraimackaton/TheDrive_riderSide.java
@@ -8,25 +8,25 @@ import android.view.View;
 import android.widget.Button;
 
 public class TheDrive_riderSide extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_the_drive_rider_side);
-
-        Button button = findViewById(R.id.navigate_button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String url =
-                        "https://www.google.com/maps/dir/?api=1&origin=32.1258379%2C+34.7975654&destination=32.1584095%2C+34.9765118&waypoints=31.7792358%2C+35.2233658%7C31.65868%2C+35.144089";
-
-                Intent i = new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse(url));
-                startActivity(i);
-            }
-        });
-    }
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_the_drive_rider_side);
+//
+//        Button button = findViewById(R.id.navigate_button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String url =
+//                        "https://www.google.com/maps/dir/?api=1&origin=32.1258379%2C+34.7975654&destination=32.1584095%2C+34.9765118&waypoints=31.7792358%2C+35.2233658%7C31.65868%2C+35.144089";
+//
+//                Intent i = new Intent(Intent.ACTION_VIEW);
+//                i.setData(Uri.parse(url));
+//                startActivity(i);
+//            }
+//        });
+//    }
 
 
 }
--- app/src/main/java/com/example/hraimackaton/gotDrive.java
+++ app/src/main/java/com/example/hraimackaton/gotDrive.java
@@ -2,17 +2,18 @@ package com.example.hraimackaton;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
-public class RiderScreen extends InputActivity {
+
+public class gotDrive extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rider_screen);
+        setContentView(R.layout.activity_got_drive);
 
         Button button = findViewById(R.id.navigate_button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -28,13 +29,5 @@ public class RiderScreen extends InputActivity {
         });
     }
 
-    public boolean isDriver() {
-        return false;
-    }
 
-
-//    public void found(View view) {
-//        Intent intent = new Intent(this, FoundDriver1.class);
-//        startActivity(intent);
-//    }
 }
